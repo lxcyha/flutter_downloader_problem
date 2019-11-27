d94840b409485a348e79360f7d4a8234be5ae68a
lxcyha <253314157@qq.com>
2019-11-27 22:57:07 +0800

    first

---
+++ lib/main.dart
@@ -0,0 +1,419 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
+import 'package:flutter_sound/flutter_sound.dart';
+
+void main() async {
+//  await FlutterDownloader.initialize();
+
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final platform = Theme.of(context).platform;
+
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(
+        title: 'Downloader',
+        platform: platform,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget with WidgetsBindingObserver {
+  final TargetPlatform platform;
+
+  MyHomePage({Key key, this.title, this.platform}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _documents = [
+    {
+      'name': 'Learning Android Studio',
+      'link':
+          'http://barbra-coco.dyndns.org/student/learning_android_studio.pdf'
+    },
+    {
+      'name': 'Android Programming Cookbook',
+      'link':
+          'http://enos.itcollege.ee/~jpoial/allalaadimised/reading/Android-Programming-Cookbook.pdf'
+    },
+    {
+      'name': 'iOS Programming Guide',
+      'link':
+          'http://darwinlogic.com/uploads/education/iOS_Programming_Guide.pdf'
+    },
+    {
+      'name': 'Objective-C Programming (Pre-Course Workbook',
+      'link':
+          'https://www.bignerdranch.com/documents/objective-c-prereading-assignment.pdf'
+    },
+  ];
+
+  final _images = [
+    {
+      'name': 'Arches National Park',
+      'link':
+          'https://upload.wikimedia.org/wikipedia/commons/6/60/The_Organ_at_Arches_National_Park_Utah_Corrected.jpg'
+    },
+    {
+      'name': 'Canyonlands National Park',
+      'link':
+          'https://upload.wikimedia.org/wikipedia/commons/7/78/Canyonlands_National_Park%E2%80%A6Needles_area_%286294480744%29.jpg'
+    },
+    {
+      'name': 'Death Valley National Park',
+      'link':
+          'https://upload.wikimedia.org/wikipedia/commons/b/b2/Sand_Dunes_in_Death_Valley_National_Park.jpg'
+    },
+    {
+      'name': 'Gates of the Arctic National Park and Preserve',
+      'link':
+          'https://upload.wikimedia.org/wikipedia/commons/e/e4/GatesofArctic.jpg'
+    }
+  ];
+
+  final _videos = [
+    {
+      'name': 'Big Buck Bunny',
+      'link':
+          'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    },
+    {
+      'name': 'Elephant Dream',
+      'link':
+          'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+    }
+  ];
+
+  List<_TaskInfo> _tasks;
+  List<_ItemHolder> _items;
+  bool _isLoading;
+  bool _permissionReady;
+  String _localPath;
+  ReceivePort _port = ReceivePort();
+
+  @override
+  void initState() {
+    super.initState();
+
+//    _bindBackgroundIsolate();
+//
+//    FlutterDownloader.registerCallback(downloadCallback);
+//
+//    _isLoading = true;
+//    _permissionReady = false;
+//
+//    _prepare();
+  }
+
+  @override
+  void dispose(){
+//    _unbindBackgroundIsolate();
+    super.dispose();
+  }
+
+  void _bindBackgroundIsolate() {
+    bool isSuccess = IsolateNameServer.registerPortWithName(
+        _port.sendPort, 'downloader_send_port');
+    if (!isSuccess) {
+      _unbindBackgroundIsolate();
+      _bindBackgroundIsolate();
+      return;
+    }
+    _port.listen((dynamic data) {
+      print('UI Isolate Callback: $data');
+      String id = data[0];
+      DownloadTaskStatus status = data[1];
+      int progress = data[2];
+
+      final task = _tasks?.firstWhere((task) => task.taskId == id);
+      if (task != null) {
+        setState(() {
+          task.status = status;
+          task.progress = progress;
+        });
+      }
+    });
+  }
+
+  void _unbindBackgroundIsolate() {
+    IsolateNameServer.removePortNameMapping('downloader_send_port');
+  }
+
+  static void downloadCallback(
+      String id, DownloadTaskStatus status, int progress) {
+    print(
+        'Background Isolate Callback: task ($id) is in status ($status) and process ($progress)');
+    final SendPort send =
+        IsolateNameServer.lookupPortByName('downloader_send_port');
+    send.send([id, status, progress]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: RaisedButton(onPressed: ()async {
+        FlutterSound flutterSound =  FlutterSound();
+
+        flutterSound.setDbLevelEnabled(true);
+        flutterSound.setDbPeakLevelUpdate(1);
+        await flutterSound.startRecorder(null);
+
+        flutterSound.onRecorderDbPeakChanged.listen((c){
+          // only call 1 time
+          print('change' + c.toString() );
+        });
+
+        Future.delayed(Duration(seconds: 10), (){
+          flutterSound.stopRecorder();
+        });
+
+      }),
+    );
+
+  }
+
+  Widget _buildActionForTask(_TaskInfo task) {
+    if (task.status == DownloadTaskStatus.undefined) {
+      return new RawMaterialButton(
+        onPressed: () {
+          _requestDownload(task);
+        },
+        child: new Icon(Icons.file_download),
+        shape: new CircleBorder(),
+        constraints: new BoxConstraints(minHeight: 32.0, minWidth: 32.0),
+      );
+    } else if (task.status == DownloadTaskStatus.running) {
+      return new RawMaterialButton(
+        onPressed: () {
+          _pauseDownload(task);
+        },
+        child: new Icon(
+          Icons.pause,
+          color: Colors.red,
+        ),
+        shape: new CircleBorder(),
+        constraints: new BoxConstraints(minHeight: 32.0, minWidth: 32.0),
+      );
+    } else if (task.status == DownloadTaskStatus.paused) {
+      return new RawMaterialButton(
+        onPressed: () {
+          _resumeDownload(task);
+        },
+        child: new Icon(
+          Icons.play_arrow,
+          color: Colors.green,
+        ),
+        shape: new CircleBorder(),
+        constraints: new BoxConstraints(minHeight: 32.0, minWidth: 32.0),
+      );
+    } else if (task.status == DownloadTaskStatus.complete) {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          new Text(
+            'Ready',
+            style: new TextStyle(color: Colors.green),
+          ),
+          RawMaterialButton(
+            onPressed: () {
+              _delete(task);
+            },
+            child: Icon(
+              Icons.delete_forever,
+              color: Colors.red,
+            ),
+            shape: new CircleBorder(),
+            constraints: new BoxConstraints(minHeight: 32.0, minWidth: 32.0),
+          )
+        ],
+      );
+    } else if (task.status == DownloadTaskStatus.canceled) {
+      return new Text('Canceled', style: new TextStyle(color: Colors.red));
+    } else if (task.status == DownloadTaskStatus.failed) {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          new Text('Failed', style: new TextStyle(color: Colors.red)),
+          RawMaterialButton(
+            onPressed: () {
+              _retryDownload(task);
+            },
+            child: Icon(
+              Icons.refresh,
+              color: Colors.green,
+            ),
+            shape: new CircleBorder(),
+            constraints: new BoxConstraints(minHeight: 32.0, minWidth: 32.0),
+          )
+        ],
+      );
+    } else {
+      return null;
+    }
+  }
+
+  void _requestDownload(_TaskInfo task) async {
+    task.taskId = await FlutterDownloader.enqueue(
+        url: task.link,
+        headers: {"auth": "test_for_sql_encoding"},
+        savedDir: _localPath,
+        showNotification: true,
+        openFileFromNotification: true);
+  }
+
+  void _cancelDownload(_TaskInfo task) async {
+    await FlutterDownloader.cancel(taskId: task.taskId);
+  }
+
+  void _pauseDownload(_TaskInfo task) async {
+    await FlutterDownloader.pause(taskId: task.taskId);
+  }
+
+  void _resumeDownload(_TaskInfo task) async {
+    String newTaskId = await FlutterDownloader.resume(taskId: task.taskId);
+    task.taskId = newTaskId;
+  }
+
+  void _retryDownload(_TaskInfo task) async {
+    String newTaskId = await FlutterDownloader.retry(taskId: task.taskId);
+    task.taskId = newTaskId;
+  }
+
+  Future<bool> _openDownloadedFile(_TaskInfo task) {
+    return FlutterDownloader.open(taskId: task.taskId);
+  }
+
+  void _delete(_TaskInfo task) async {
+    await FlutterDownloader.remove(
+        taskId: task.taskId, shouldDeleteContent: true);
+    await _prepare();
+    setState(() {});
+  }
+
+  Future<bool> _checkPermission() async {
+    if (widget.platform == TargetPlatform.android) {
+      PermissionStatus permission = await PermissionHandler()
+          .checkPermissionStatus(PermissionGroup.storage);
+      if (permission != PermissionStatus.granted) {
+        Map<PermissionGroup, PermissionStatus> permissions =
+            await PermissionHandler()
+                .requestPermissions([PermissionGroup.storage]);
+        if (permissions[PermissionGroup.storage] == PermissionStatus.granted) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return true;
+    }
+    return false;
+  }
+
+  Future<Null> _prepare() async {
+    final tasks = await FlutterDownloader.loadTasks();
+
+    int count = 0;
+    _tasks = [];
+    _items = [];
+
+    _tasks.addAll(_documents.map((document) =>
+        _TaskInfo(name: document['name'], link: document['link'])));
+
+    _items.add(_ItemHolder(name: 'Documents'));
+    for (int i = count; i < _tasks.length; i++) {
+      _items.add(_ItemHolder(name: _tasks[i].name, task: _tasks[i]));
+      count++;
+    }
+
+    _tasks.addAll(_images
+        .map((image) => _TaskInfo(name: image['name'], link: image['link'])));
+
+    _items.add(_ItemHolder(name: 'Images'));
+    for (int i = count; i < _tasks.length; i++) {
+      _items.add(_ItemHolder(name: _tasks[i].name, task: _tasks[i]));
+      count++;
+    }
+
+    _tasks.addAll(_videos
+        .map((video) => _TaskInfo(name: video['name'], link: video['link'])));
+
+    _items.add(_ItemHolder(name: 'Videos'));
+    for (int i = count; i < _tasks.length; i++) {
+      _items.add(_ItemHolder(name: _tasks[i].name, task: _tasks[i]));
+      count++;
+    }
+
+    tasks?.forEach((task) {
+      for (_TaskInfo info in _tasks) {
+        if (info.link == task.url) {
+          info.taskId = task.taskId;
+          info.status = task.status;
+          info.progress = task.progress;
+        }
+      }
+    });
+
+    _permissionReady = await _checkPermission();
+
+    _localPath = (await _findLocalPath()) + '/Download';
+
+    final savedDir = Directory(_localPath);
+    bool hasExisted = await savedDir.exists();
+    if (!hasExisted) {
+      savedDir.create();
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Future<String> _findLocalPath() async {
+    final directory = widget.platform == TargetPlatform.android
+        ? await getExternalStorageDirectory()
+        : await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+}
+
+class _TaskInfo {
+  final String name;
+  final String link;
+
+  String taskId;
+  int progress = 0;
+  DownloadTaskStatus status = DownloadTaskStatus.undefined;
+
+  _TaskInfo({this.name, this.link});
+}
+
+class _ItemHolder {
+  final String name;
+  final _TaskInfo task;
+
+  _ItemHolder({this.name, this.task});
+}
